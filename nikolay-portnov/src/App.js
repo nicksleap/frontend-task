6a892e10dd4f8bb7702642f46ff99c6b5c9b4450
Nikolay Portnov <nicksleap@yandex.ru>
2020-02-19 13:32:05 +0400

    Добавил стлили для боди и вывел лайауты на экран

--- nikolay-portnov/src/App.js
+++ nikolay-portnov/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './assets/App.css';
+import Promo from './layouts/Promo';
+import Contact from './layouts/Contact';
+import Contact1 from './layouts/Contact_1';
+import Select from './layouts/Select';
+import Order from './layouts/Order';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-
+      <Promo/>
+      <Contact/>
+      <Contact1/>
+      <Select/>
+      <Order/>
     </div>
   );
-}
+};
 
 export default App;
