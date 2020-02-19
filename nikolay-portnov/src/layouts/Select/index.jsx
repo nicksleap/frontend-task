af1b849950b4749ed28fc5a8e8369849c551d828
Nikolay Portnov <nicksleap@yandex.ru>
2020-02-19 13:00:50 +0400

    Добавил разметку для выбора бутылей

---
+++ nikolay-portnov/src/layouts/Select/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Select = () => {
+  return (
+    <div>
+      <button><span>Назад</span></button>
+      <h2>Вода</h2>
+      {/* Select bottle */}
+      {/* Select date */}
+      {/* Select time */}
+      <button>Заказать воду</button>
+    </div>
+  );
+};
+
+export default Select;
