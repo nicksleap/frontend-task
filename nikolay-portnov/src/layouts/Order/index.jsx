00a7cec195c16b6089979d7ca06c13cc79fe99af
Nikolay Portnov <nicksleap@yandex.ru>
2020-02-19 13:14:15 +0400

    Добавил ордер для формы

---
+++ nikolay-portnov/src/layouts/Order/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Order = () => {
+  return (
+    <div>
+      <h2>Заказ оформлен</h2>
+      {/* Draft */}
+      {/* Amount */}
+      <div>
+        Ваш заказ №{'221'} успешно оформлен.
+        В ближайшее время по указанному телефону с
+        Вами свяжется наш менеджер.</div>
+      <button>Новый заказ</button>
+    </div>
+  );
+};
+
+export default Order;
