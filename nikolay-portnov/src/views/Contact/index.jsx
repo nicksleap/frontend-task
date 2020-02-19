506c33791feb4cccdbee13db21869b2570109d48
Nikolay Portnov <nicksleap@yandex.ru>
2020-02-19 12:05:09 +0400

    Добавил для вью первого шага формы

---
+++ nikolay-portnov/src/views/Contact/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Заполните данные</h2>
+      {/* inputs */}
+      <button>Далее</button>
+    </div>
+  )
+};
+
+export default Contact;
